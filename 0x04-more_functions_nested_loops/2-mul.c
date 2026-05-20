97279271d7c09f644a4b82549d3dc21cd8c9c4c7
Samuel Bereket <samuelbereket123@gmail.com>
2026-05-20 17:11:26 +0300

    first attempt

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul -checks for two multiplied integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return a * b;
+}

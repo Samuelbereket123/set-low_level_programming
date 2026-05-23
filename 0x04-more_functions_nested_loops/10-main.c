32fc405a28fbc2289e6e3b0ab8f4c736c55a0c77
Samuel Bereket <samuelbereket123@gmail.com>
2026-05-23 17:35:39 +0300

    initial commit

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void) 
+{
+	print_trianlge(2);
+	print_triangle(10);
+	print_trianlge(1);
+	return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(' ');
+		}
+
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}

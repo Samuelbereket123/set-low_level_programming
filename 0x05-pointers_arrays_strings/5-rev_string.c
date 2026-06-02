a110f35bce28a294edf62a84019afeceb16c7771
Samuel Bereket <samuelbereket123@gmail.com>
2026-06-03 00:58:41 +0300

    initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = 0;
+	char temp;
+
+	while (s[end] != '\0')
+	{
+		end++;
+	}
+
+	end--;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}

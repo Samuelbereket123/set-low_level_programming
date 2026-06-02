9781d10adad5ea7bb88b9e997e5be4dc07fa8e56
Samuel Bereket <samuelbereket123@gmail.com>
2026-06-02 14:33:06 +0300

    fixed betty-style-documentation

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
-
-
+/**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to an integer
+ *
+ * Return: nothing
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
-

5b709804e3f0c9d19a31a3e2d6050b10b651e0e6
Samuel Bereket <samuelbereket123@gmail.com>
2026-06-04 04:59:18 +0300

    initial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int start;
+
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		start = len / 2;
+	} 
+	else 
+	{
+		start = (len + 1) / 2;
+	}
+	
+	while(str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+
+	_putchar('\n');
+	
+}

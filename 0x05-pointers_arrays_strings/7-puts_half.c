4a18c7966142dc17c97c933a4dbdb8e85de3d660
Samuel Bereket <samuelbereket123@gmail.com>
2026-06-04 05:03:53 +0300

    enhanced to betty sytle of documentation

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to the string
+ *
+ * Return: nothing
+ */
+
 void puts_half(char *str)
 {
 	int len = 0;

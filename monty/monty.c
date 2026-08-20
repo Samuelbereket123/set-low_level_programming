126bac007e0f25e49f7ae7972fb584aee99b0acc
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-20 16:52:53 +0300

    monty initial

---
+++ monty/monty.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: EXIT_SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+	return (EXIT_SUCCESS);
+}

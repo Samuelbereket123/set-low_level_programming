91483bac654d4c7f89d2de5f8de4c7b94d12b989
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-20 12:03:42 +0300

    100

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -33,7 +33,32 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
 
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+shash_node_t *shash_create_node(const char *key, const char *value);
+int shash_update(shash_node_t *node, const char *value);
+void shash_sorted_insert(shash_table_t *ht, shash_node_t *node);
 
 
 

a3365ff761313022241760909aa7763e4a7e2b77
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-20 11:34:45 +0300

    hash tables

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,30 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+/**
+ * struct hash_node_s - node of a hash table
+ * @key: the key
+ * @value: the value associated with the key
+ * @next: points to the next node of the list
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - hash table data structure
+ * @size: the size of the array
+ * @array: an array of size @size
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif

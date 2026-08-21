2482104745dc6c34c7ca69894a58d3276ef05713
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-21 03:06:58 +0300

    9 fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,8 +18,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
 	if (idx == 0)
-		return (add_nodeint(head, n));
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
 	current = *head;
 	i = 0;
@@ -31,13 +41,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	if (current == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
 	new_node->next = current->next;
 	current->next = new_node;
 

a6e2bdfd52195ff458bdc7a0422b2a89a8b4d814
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-21 04:33:58 +0300

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	dlistint_t *next;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}

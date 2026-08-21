9c927db0375f7f671214abab65a685901c85f47f
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-21 03:58:57 +0300

    102 fix

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 
 /**
+ * add_node_to_array - adds a node to an array
+ * @nodes: array of node pointers
+ * @count: number of nodes
+ * @node: node to add
+ *
+ * Return: new array
+ */
+listint_t **add_node_to_array(listint_t **nodes, size_t count,
+			      listint_t *node)
+{
+	listint_t **new_nodes;
+	size_t i;
+
+	new_nodes = malloc((count + 1) * sizeof(*new_nodes));
+	if (new_nodes == NULL)
+	{
+		free(nodes);
+		exit(98);
+	}
+
+	i = 0;
+	while (i < count)
+	{
+		new_nodes[i] = nodes[i];
+		i++;
+	}
+
+	new_nodes[count] = node;
+	free(nodes);
+
+	return (new_nodes);
+}
+
+/**
  * free_listint_safe - frees a listint_t list safely
  * @h: pointer to the head of the list
  *
@@ -12,7 +46,6 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *current;
 	listint_t *next;
 	listint_t **nodes;
-	listint_t **new_nodes;
 	size_t count;
 	size_t i;
 
@@ -38,25 +71,7 @@ size_t free_listint_safe(listint_t **h)
 		}
 
 		next = current->next;
-
-		new_nodes = malloc((count + 1) * sizeof(*new_nodes));
-		if (new_nodes == NULL)
-		{
-			free(nodes);
-			exit(98);
-		}
-
-		i = 0;
-		while (i < count)
-		{
-			new_nodes[i] = nodes[i];
-			i++;
-		}
-
-		new_nodes[count] = current;
-		free(nodes);
-		nodes = new_nodes;
-
+		nodes = add_node_to_array(nodes, count, current);
 		free(current);
 		count++;
 		current = next;
